17e603ffb116b8466662cc2391bb37dc57fbc920
Peter Salz <psalz@web.de>
2019-10-16 22:51:42 +0200

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Peter Salz on 16.10.19.
+//  Copyright © 2019 Peter Salz App Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
